724a3b56bd4a1beb70afdda73e930fc7b54a945d
Kakushi <70345058+kakushi-w@users.noreply.github.com>
2026-03-17 23:51:06 +0800

    创建邀请api

--- web/views/api/users.py
+++ web/views/api/users.py
@@ -4,7 +4,50 @@ from shared_data import shared_users
 from . import APIView, APIError
 
 from web.models import ActionLogEntry
-
+from django.utils.http import urlsafe_base64_encode  
+from django.utils.encoding import force_bytes  
+from web.views.invite import account_activation_token  
+from web.models.site import get_current_site  
+from django.contrib.auth import get_user_model  
+  
+User = get_user_model()  
+  
+class GenerateInviteLinkView(APIView):  
+    @takes_json  
+    def post(self, request: HttpRequest):  
+        if not request.user.has_perm('roles.manage_users'):  
+            raise APIError('权限不足', 403)  
+          
+        email = self.json_input.get('email')  
+        if not email:  
+            raise APIError('未指定邮箱', 400)  
+          
+        roles = self.json_input.get('roles', [])  
+          
+        try:  
+            user, created = User.objects.get_or_create(email=email)  
+            if not created:  
+                raise APIError('该邮箱的用户已存在', 409)  
+              
+            if roles:  
+                user.roles.set(roles)  
+            user.is_active = False  
+            user.username = f'user-{user.id}'  
+            user.save()  
+               
+            token = account_activation_token.make_token(user)  
+            uid = urlsafe_base64_encode(force_bytes(user.pk))  
+            site = get_current_site()  
+            invitation_url = f"{request.scheme}://{site.domain}/-/accept/{uid}/{token}"  
+              
+            return self.render_json(200, {  
+                'email': email,  
+                'invitationUrl': invitation_url,  
+                'userId': user.id  
+            })  
+              
+        except Exception as e:  
+            raise APIError('创建邀请时出错', 500)
 
 class AllUsersView(APIView):
     def get(self, request: HttpRequest):
