4c24d0c045e484d8b96c5bb3a983eee3fe437ca6
Kakushi <70345058+kakushi-w@users.noreply.github.com>
2026-09-08 16:07:43 +0900

    remove the python modules and renderer

--- modules/_csrf_protection.py
+++
@@ -1,3 +0,0 @@
-def csrf_safe_method(func):
-    setattr(func, 'is_csrf_safe', True)
-    return func
--- modules/pagedescription.py
+++
@@ -1,9 +0,0 @@
-def has_content():
-    return True
-
-
-def render(context, _params, content=''):
-    content = content.replace('\u00a0', ' ').replace('<', '\\u003c')
-    if content:
-        context.og_description = content
-    return ''
